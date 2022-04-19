f2c29b0d869f1ae860f183b8e2ed95f1d4fb8508
Juan Carlos Garrote <juanca_arrabal@hotmail.com>
2022-04-19 11:54:15 +0200

    Initial Compose structure

---
+++ owncloudApp/src/main/java/com/owncloud/android/presentation/ui/sharing/fragments/ShareFileComposeFragment.kt
@@ -0,0 +1,26 @@
+package com.owncloud.android.presentation.ui.sharing.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+
+class ShareFileComposeFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+                Text("Hello ownCloud")
+                // Compose code
+            }
+        }
+    }
+}
