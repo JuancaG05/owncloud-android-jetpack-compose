82adf72ab0c4fdfc9315a63d6b4a7218fe21398c
Juan Carlos Garrote <juanca_arrabal@hotmail.com>
2022-04-28 13:00:51 +0200

    File part of the ShareFileFragment moved to Compose

--- owncloudApp/src/main/java/com/owncloud/android/presentation/ui/sharing/fragments/ShareFileComposeFragment.kt
+++ owncloudApp/src/main/java/com/owncloud/android/presentation/ui/sharing/fragments/ShareFileComposeFragment.kt
@@ -4,12 +4,48 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
+import com.owncloud.android.R
+import com.owncloud.android.datamodel.OCFile
+import com.owncloud.android.datamodel.ThumbnailsCacheManager
+import com.owncloud.android.utils.DisplayUtils
+import com.owncloud.android.utils.MimetypeIconUtil
 
 class ShareFileComposeFragment: Fragment() {
+
+    private var file: OCFile? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            file = it.getParcelable(ARG_FILE)
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,9 +54,73 @@ class ShareFileComposeFragment: Fragment() {
         return ComposeView(requireContext()).apply {
             setContent {
                 setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-                Text("Hello ownCloud")
-                // Compose code
+                Column(
+                    modifier = Modifier.verticalScroll(rememberScrollState())
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(dimensionResource(id = R.dimen.standard_padding))
+                    ) {
+                        val thumbnail = ThumbnailsCacheManager.getBitmapFromDiskCache(file?.remoteId.toString())
+                        if (file!!.isImage && thumbnail != null) {
+                            Image(
+                                bitmap = thumbnail.asImageBitmap(),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .size(dimensionResource(id = R.dimen.file_icon_size))
+                                    .fillMaxSize()
+                            )
+                        } else {
+                            Image(
+                                painter = painterResource(id = MimetypeIconUtil.getFileTypeIconId(file?.mimetype, file?.fileName)),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .size(dimensionResource(id = R.dimen.file_icon_size))
+                                    .fillMaxSize()
+                            )
+                        }
+                        Column(
+                            modifier = Modifier.weight(1f).padding(start = 12.dp)
+                        ) {
+                            Text(
+                                text = file?.fileName!!,
+                                fontSize = 16.sp,
+                                color = colorResource(id = R.color.black),
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                modifier = Modifier.padding(end = dimensionResource(id = R.dimen.standard_half_margin))
+                            )
+                            if (!file!!.isFolder) {
+                                Text(
+                                    text = DisplayUtils.bytesToHumanReadable(file!!.fileLength, activity),
+                                    fontSize = 12.sp,
+                                    color = colorResource(id = R.color.half_black),
+                                )
+                            }
+                        }
+                        Image(
+                            painter = painterResource(id = R.drawable.copy_link),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(28.dp)
+                                .padding(end = dimensionResource(id = R.dimen.standard_half_padding))
+                        )
+                    }
+                }
             }
+        }
+    }
+
+    companion object {
+        private const val ARG_FILE = "FILE"
+
+        fun newInstance(
+            fileToShare: OCFile
+        ): ShareFileComposeFragment {
+            val args = Bundle().apply {
+                putParcelable(ARG_FILE, fileToShare)
+            }
+            return ShareFileComposeFragment().apply { arguments = args }
         }
     }
 }
